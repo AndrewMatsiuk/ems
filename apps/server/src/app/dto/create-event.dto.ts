62f053f1eb318e527eb5bde4c4778c404989ac56
AndriiMatsiuk <amacyuk2@gmail.com>
2025-03-31 13:49:31 +0300

    update dto

--- apps/server/src/app/dto/create-event.dto.ts
+++ apps/server/src/app/dto/create-event.dto.ts
@@ -1,17 +1,27 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsString, IsOptional, IsUrl } from 'class-validator';
 
 export class CreateEventDto {
   @IsNotEmpty()
+  @IsString()
   title!: string;
 
   @IsNotEmpty()
+  @IsString()
   date!: string;
 
   @IsNotEmpty()
+  @IsString()
   location!: string;
 
   @IsNotEmpty()
+  @IsString()
   category!: string;
 
+  @IsOptional()
+  @IsString()
   description?: string;
+
+  @IsOptional()
+  @IsUrl({}, { message: 'imageUrl must be a valid URL' })
+  imageUrl?: string;
 }
